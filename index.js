531644975277fa4f7351195148991002a99ad4fe
James1994ny <James1994ny@gmail.com>
2017-01-23 22:43:44 -0500

    Updated ports 2 and updated pg.connect stuff

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ res.render('index')
 
 //projects page
 app.get('/projects', function(req, res){      //Once node index.js is running. http://localhost:3000/ should return the page. hence the '/' which referres to just localhost:300 page
-  pg.connect('postgres://postgres:12345@localhost:5432/portfolio', function(err, client, done){ //user name is postgres: and password is 12345  \conninfo on psql to get the username info. \password to change the password database is called test
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){ //user name is postgres: and password is 12345  \conninfo on psql to get the username info. \password to change the password database is called test
           client.query('SELECT * FROM PROJECTS', function(err, result) {          //Selects * from the project table aka sql command
               res.render('projects',{ data: result.rows});                       //Links my projects.ejs file and shows data related to that file.
             console.log(result.row)
@@ -41,7 +41,7 @@ app.get('/projects', function(req, res){      //Once node index.js is running. h
 
       // post a blog on about.ejs
       app.post('/about', function(req,res){  //Reads stuff from the locahost location /
-        pg.connect('postgres://postgres:12345@localhost:5432/portfolio', function(err, client, done){ //Connects to the database
+        pg.connect(process.env.DATABASE_URL, function(err, client, done){ //Connects to the database
           client.query(`insert into post(title,body) values('${req.body.title}','${req.body.body}')` , function(err, result){  //inserts into the message table. Note the ${req.body.title etc is form the body-parser.
             //We referenced body parser above. It automatically looks into my views ejs folders and finds form names with "title" and "body" and adds it to my message table.
             console.log(err);
@@ -55,7 +55,7 @@ app.get('/projects', function(req, res){      //Once node index.js is running. h
 
       // retrives info on /about page
       app.get('/about', function(req, res){      //Once node index.js is running. http://localhost:3000/ should return the page. hence the '/' which referres to just localhost:300 page
-        pg.connect('postgres://postgres:12345@localhost:5432/portfolio', function(err, client, done){ //user name is postgres: and password is 12345  \conninfo on psql to get the username info. \password to change the password database is called test
+        pg.connect(process.env.DATABASE_URL, function(err, client, done){ //user name is postgres: and password is 12345  \conninfo on psql to get the username info. \password to change the password database is called test
           client.query('SELECT * FROM POST', function(err, result) {          //Selects * from the jmztable aka sql command
             res.render('about', { data: result.rows});                       //Links my home.ejs file and shows data related to that file.
             console.log(result.rows)
@@ -68,7 +68,7 @@ app.get('/projects', function(req, res){      //Once node index.js is running. h
 
 //Click on title to pull up project info.
 app.get('/projects/:id', function(req,res){  //Creates a page for http://localhost:3000/1 2 3 ....
-    pg.connect('postgres://postgres:12345@localhost:5432/portfolio', function(err, client, done){ //Connects to the database
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){ //Connects to the database
     var project_id = req.params.id //the req.params.id referes to the id value of /:id
     console.log(project_id)
     client.query(`select * from projects where id = '${project_id}'`, function(err,result){
@@ -82,7 +82,7 @@ app.get('/projects/:id', function(req,res){  //Creates a page for http://localho
 
 //Click on title to pull up about blog post info.
 app.get('/about/:id', function(req,res){  //Creates a page for http://localhost:3000/1 2 3 ....
-    pg.connect('postgres://postgres:12345@localhost:5432/portfolio', function(err, client, done){ //Connects to the database
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){ //Connects to the database
     var post_id = req.params.id //the req.params.id referes to the id value of /:id
     console.log(post_id)
     client.query(`select * from post where id = '${post_id}'`, function(err,result){
